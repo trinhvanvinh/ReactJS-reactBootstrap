6ca5f4933ec34f51225bb0b47d9c9ff65ba84dfa
mac <>
2019-05-09 16:06:10 +0700

    resource

---
+++ react_bootstrap/src/App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import News from './components/News/News';
+import Navbar from './components/Utils/CustomNavbar';
+
+class App extends Component {
+  render(){
+    return (
+      <Router>
+        <div>
+          <Navbar/>
+          <Route exact path="/" component ={Home} />
+          <Route  path="/about" component ={About} />
+          <Route  path="/news" component ={News} />
+        </div>
+      </Router>
+    );
+  } 
+}
+
+export default App;
---
+++ react_bootstrap/src/components/Utils/CustomNavbar.js
@@ -0,0 +1,33 @@
+
+import React, { Component } from 'react'
+import { Navbar, Nav, NavItem, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './CustomNavbar.css'
+
+export default class CustomNavbar extends Component {
+  render() {
+    return (
+      <Navbar default collapseOnSelect>
+    
+          <Navbar.Brand>
+            <Link to="/">CodeLife</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem eventKey={1} componentClass={Link} href="/" to="/">
+              Home
+            </NavItem>
+            <NavItem eventKey={2} componentClass={Link} href="/about" to="/about">
+              About
+            </NavItem>
+            <NavItem eventKey={3} componentClass={Link} href="/news" to="/news">
+              News
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    )
+  }
+}
